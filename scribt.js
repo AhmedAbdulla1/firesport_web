de2ce8dc54522f3a09ac7916d5178516aaa6fd5d
Astr0n1 <astron0learn@gmail.com>
2025-02-02 23:33:04 +0200

    secure key try 1

--- scribt.js
+++ scribt.js
@@ -1,15 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
+import firebaseConfig from "./firebaseConfig.js"
 
-const firebaseConfig = {
-    apiKey: "AIzaSyD3hgVF0VctPHjVWHW7E0r4Ed9pTaH6yFo",
-    authDomain: "firebox-349ab.firebaseapp.com",
-    projectId: "firebox-349ab",
-    storageBucket: "firebox-349ab.appspot.com",
-    messagingSenderId: "144165828319",
-    appId: "1:144165828319:web:677a4d751868516b0ddb6a",
-    measurementId: "G-HZMZEP4W20"
-  };
+
   
 // 🔹 تهيئة Firebase
 const app = initializeApp(firebaseConfig);
