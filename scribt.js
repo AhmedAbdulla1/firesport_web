b79ed97e1b7997779be26b8dab5a2e15db6aa6e3
Astr0n1 <astron0learn@gmail.com>
2025-02-02 23:45:04 +0200

    unSecure keys

--- scribt.js
+++ scribt.js
@@ -1,13 +1,20 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
 import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-firestore.js";
-import firebaseConfig from "./firebaseConfig.js"
 
-
-  
 // 🔹 تهيئة Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyD3hgVF0VctPHjVWHW7E0r4Ed9pTaH6yFo",
+  authDomain: "firebox-349ab.firebaseapp.com",
+  projectId: "firebox-349ab",
+  storageBucket: "firebox-349ab.appspot.com",
+  messagingSenderId: "144165828319",
+  appId: "1:144165828319:web:677a4d751868516b0ddb6a",
+  measurementId: "G-HZMZEP4W20"
+};
+
+
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
 // 🔹 دالة لإضافة بيانات
 const addUser = async () => {
   try {
